d3fc2e58d77e511f556113d61300fbb3a6167602
Sayani Koley <sayanikoley8@gmail.com>
2021-06-28 10:17:51 +0530

    Ability to check valid last name

--- UserRegistrationProblem/src/UserRegistration.java
+++ UserRegistrationProblem/src/UserRegistration.java
@@ -1,5 +1,5 @@
 /**
- * Valid First Name with first letter starting with capital and has minimum 3 characters in it.
+ * Valid Last Name with first letter starting with capital and has minimum 3 characters in it.
  *
  * @author: SAYANI KOLEY
  * @since: 28.06.2021
@@ -9,9 +9,15 @@ public class UserRegistration {
     public static void main(String[] args) {
         System.out.println(firstName("Tom"));
         System.out.println(firstName("Harry"));
+        System.out.println(lastName("Hanks"));
+        System.out.println(lastName("Abc"));
     }
     // validate first name
     public static boolean firstName( String firstName ) {
+        return firstName.matches("^[A-Z]{1}[a-z]{2,}$");
+    }
+    // validate first name
+    public static boolean lastName( String firstName ) {
         return firstName.matches("^[A-Z]{1}[a-z]{2,}$");
     }
 }
