392bfe7e1ff5f8611873da59d25a483e2aa7e792
Sayani Koley <sayanikoley8@gmail.com>
2021-06-28 10:53:19 +0530

    Ability to validate email

--- UserRegistrationProblem/src/UserRegistration.java
+++ UserRegistrationProblem/src/UserRegistration.java
@@ -1,6 +1,8 @@
 /**
- * Valid Last Name with first letter starting with capital and has minimum 3 characters in it.
- *
+ * Check valid email
+ * e.g. abc.xyz@bl.co.in
+ * 3 mandatory parts(abc, bl, co) and 2 optional parts(xyz, in) with precise @ and . positions.
+ * 
  * @author: SAYANI KOLEY
  * @since: 28.06.2021
  */
@@ -9,8 +11,11 @@ public class UserRegistration {
     public static void main(String[] args) {
         System.out.println(firstName("Tom"));
         System.out.println(firstName("Harry"));
+
         System.out.println(lastName("Hanks"));
         System.out.println(lastName("Abc"));
+
+        email();
     }
     // validate first name
     public static boolean firstName( String firstName ) {
@@ -19,5 +24,16 @@ public class UserRegistration {
     // validate first name
     public static boolean lastName( String firstName ) {
         return firstName.matches("^[A-Z]{1}[a-z]{2,}$");
+    }
+    //validate given email
+    public static void email() {
+        String email = "abc.xyz@bl.co.in";
+        String regex = "^[a-z]{3}[a-zA-Z0-9+_.-]*@[a-z]{2}[.]{1}[a-z]{2}[.][a-z]*$";
+        boolean result = email.matches(regex);
+        if(result) {
+            System.out.println("Given email-id is valid");
+        } else {
+            System.out.println("Given email-id is not valid");
+        }
     }
 }
